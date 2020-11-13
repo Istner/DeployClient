71639e818b1587b5b674f2079460f13fb202a909
Fran-FC <frafolcm@gmail.com>
2020-11-13 09:56:02 +0100

    changed paths

--- startup.sh
+++ startup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-CONFIG_FILE="$HOME/istner-client/config/config.json"
-
+CONFIG_FOLDER="$HOME/istner-client/config/"
+CONFIG_FILE="$HOME/config.json"
 # Write into config.json the authentication parameters and server information
+touch $CONFIG_FILE
 read -p "Nombre de usuario: " uname
 read -sp "Contraseña: " upass
-echo "{\"uName\": \"$uname\", \"uPass\": \"$upass\",\"ipServer\": \"localhost\", \"portServer\": \"8000\"}" > $HOME
+echo "{\"uName\": \"$uname\", \"uPass\": \"$upass\",\"ipServer\": \"localhost\", \"portServer\": \"8000\"}" > $CONFIG_FILE
 
 # Resolve software dependencies 
-sudo apt update; sudo apt install python3 gphoto2 git
+sudo apt update; sudo apt install python3 gphoto2 -y
 
 # Cambiar a un repositorio de la empresa
 git config user.name Fran-FC
@@ -16,3 +17,4 @@ git config user.email frafolcm@gmail.com
 
 git clone https://github.com/Fran-FC/istner-client $HOME
 
+mv $CONFIG_FILE $CONFIG_FOLDER
