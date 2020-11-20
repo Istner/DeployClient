d4bcc2aadba5e59b567a0ef1fc7f9a1057e818b1
Francesc Folch <72190914+Fran-FC@users.noreply.github.com>
2020-11-20 08:42:45 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,18 +3,14 @@ CONFIG_FOLDER="$HOME/istner-client/config/"
 CONFIG_FILE="$HOME/config.json"
 # Write into config.json the authentication parameters and server information
 touch $CONFIG_FILE
-read -p "Nombre de usuario: " uname
+read -p "Nombre de usuario para el cliente web: " uname
 read -sp "Contraseña: " upass
 echo "{\"uName\": \"$uname\", \"uPass\": \"$upass\",\"ipServer\": \"localhost\", \"portServer\": \"8000\"}" > $CONFIG_FILE
 
-# Resolve software dependencies 
-sudo apt update; sudo apt install python3 gphoto2 -y
-
 # Cambiar a un repositorio de la empresa
-# git config user.name Fran-FC
-# git config user.signingKey Estofen\!12
-# git config user.email frafolcm@gmail.com
-
 git clone https://github.com/Fran-FC/istner-client "$HOME/istner-client"
+
+# Resolve software dependencies 
+sudo apt update; sudo apt install python3 gphoto2 -y; sudo apt upgrade -y; sudo apt autoremove -y
 
 mv $CONFIG_FILE $CONFIG_FOLDER
