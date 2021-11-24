bec0015a2b0bef9e0c18fe803bbf7cfc948801e3
Istner <78016552+Istner@users.noreply.github.com>
2021-11-24 12:48:47 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,7 +12,7 @@ echo "{\"uName\": \"$uname\", \"uPass\": \"$upass\",\"conversion\": \"$conversio
 # Cambiar a un repositorio de la empresa
 #git clone https://github.com/Istner/RaspberryClient "$HOME/RaspberryClient"
 read -p "Introduzca token: " TOKEN
-git clone https://$TOKEN@github.com/Istner/RaspberryClient
+git clone https://$TOKEN@github.com/Istner/RaspberryClient/tree/client
 # Resolve software dependencies 
 sudo apt install gphoto2 arp-scan python3-pip libgphoto2-dev -y
 # sudo apt upgrade -y; sudo apt autoremove -y
